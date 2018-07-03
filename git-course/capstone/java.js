4edacae08b7623df9ea9df7bf0718e5b96b16865
Jordan Hudgens <jordan@devcamp.com>
2018-07-03 09:52:50 -0600

    Update#9

---
+++ git-course/capstone/java.js
@@ -0,0 +1,9 @@
+// const navHam = document.querySelector("#navHam");
+
+// navHam.addEventListener("click", e => {
+//   navHam.parentElement.classList.toggle("active");
+// });
+const navHam = document.querySelector("#navHam");
+navHam.addEventListener("click", e => {
+  navHam.parentElement.classList.toggle("active");
+});
