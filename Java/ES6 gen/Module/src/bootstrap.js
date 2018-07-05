214c81357875cca4f39ab513fabf91d6863f373b
Jordan Hudgens <jordan@devcamp.com>
2018-07-05 14:01:47 -0600

    JS.19.0

--- Java/ES6 gen/Module/src/bootstrap.js
+++ Java/ES6 gen/Module/src/bootstrap.js
@@ -1,3 +1,7 @@
-import { multiply } from "./WHATEVER";
+import * as WHATEVER from "./WHATEVER";
+import navigation, { greeting, multiply } from "./navigation";
 
-console.log(multiply(2, 5));
+console.log(navigation());
+console.log(WHATEVER.greeting);
+console.log(WHATEVER.multiply(5, 20));
+console.log(greeting);
