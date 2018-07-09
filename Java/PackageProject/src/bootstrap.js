b707fc6da96d43db155387e3075a4e4cb5f84865
Jordan Hudgens <jordan@devcamp.com>
2018-07-09 11:09:24 -0600

    Update: Added Browser File JS, Added Custom Snippets to VS

--- Java/PackageProject/src/bootstrap.js
+++ Java/PackageProject/src/bootstrap.js
@@ -1,1 +1,17 @@
-// ES6 code goes here
+import moment from "moment";
+
+// const sixMonthsAgo = moment().subtract(6, "months");
+
+// console.log(sixMonthsAgo.toString());
+
+// const twoWeeksFromNow = moment().add(2, "weeks");
+
+// console.log(twoWeeksFromNow.toString());
+
+// const birthday = moment("2001-11-12", "YYYY-MM-DD");
+
+// console.log(birthday.fromNow());
+
+// console.log(birthday.format("dddd"));
+
+// console.log(birthday.format("MMM Do YYYY"));
