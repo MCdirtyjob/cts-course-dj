0f7a9ad13015c317de554939a138280860260989
Jordan Hudgens <jordan@devcamp.com>
2018-07-16 10:57:27 -0600

    State

--- Week 3/madlibs/src/components/card.js
+++ Week 3/madlibs/src/components/card.js
@@ -1,9 +1,31 @@
 import React, {Component} from 'react';
 import Input from './input';
 class Card extends Component{
+
+    constructor(){
+        super()
+        this.state={
+            color: '',
+            pluralNoun: ''
+        }
+        
+    }
+    handelInputChange(){
+        this.setState({ color: 'red'})
+        
+}
+
     render(){
+
+        return(
+        <div className="card">
+            <h1>{this.state.color}</h1>
+            <input/>
+            </div>
+        )
         return(
             <div className="card">
+                <h1>{this.state.color}</h1>
                 {Input('Color')}
                 {Input('Plural Noun')}                
             </div>
