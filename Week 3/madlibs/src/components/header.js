9e5f33bb23a2b6c65f3182cadc7bc98d2e0cc606
Jordan Hudgens <jordan@devcamp.com>
2018-07-16 10:14:00 -0600

    Update: "Built functional component"

---
+++ Week 3/madlibs/src/components/header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header =() =>{
+    return(
+        <div className="header">
+            <h1>Bottega Madlibs</h1>
+            <p>Fill out the fields below and click the button to see the Mad Lib story.</p>
+        </div>
+    )
+}
+// const Header =(title, content ) =>{
+//     return(
+//         <div className="header">
+//             <h1>{title}</h1>
+//             <p>{content}</p>
+//         </div>
+//     )
+// }
+export default Header;
---
+++ Week 3/madlibs/src/components/home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import Header from "./header";
+ class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        {Header()}
+      </div>
+    );
+  }
+}
+
+// class Home extends Component {
+//   render() {
+//     return (
+//       <div className="home">
+//         {Header('Bottega Madlibs','Fill out the fields below and click the button to see the Mad Lib story.')}
+//       </div>
+//     );
+//   }
+// }
+export default Home;
