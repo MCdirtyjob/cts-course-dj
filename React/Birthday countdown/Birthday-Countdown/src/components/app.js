b70cfe3fee1e30e674d2e7ad8c3bbf331a0b7137
Jordan Hudgens <jordan@devcamp.com>
2018-07-25 09:19:36 -0600

    Added Second Skew, CSS organization

--- React/Birthday countdown/Birthday-Countdown/src/components/app.js
+++ React/Birthday countdown/Birthday-Countdown/src/components/app.js
@@ -4,9 +4,11 @@ export default class App extends Component {
   render() {
     return (
       <div className="grid">
-        <div>Yuppp</div>
-        <div>Hi</div>
-        <div>Hi</div>
+        <div className="grid__skew-dark-one-box" />
+        <div className="grid__skew-dark-two" />
+        <div className="grid__skew-dark-three" />
+        <div className="grid__skew-dark-one" />
+
         <div className="grid__skew-light-one" />
         <div className="grid__skew-light-two" />
         <div className="grid__skew-light-three-box" />
