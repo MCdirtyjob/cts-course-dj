8ee1adf8b924706fd9ebd4f00d670920f8afbd50
Jordan Hudgens <jordan@devcamp.com>
2018-07-25 11:13:13 -0600

    Installed Moment, react datepicker

---
+++ React/Birthday countdown/Birthday-Countdown/src/components/picker.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import DatePicker from "react-datepicker";
+import moment from "moment";
+
+export default class Picker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: moment()
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+  render() {
+    return;
+    <div className="picker">
+      <DatePicker
+        selected={this.state.startDate}
+        onChange={this.handleChange}
+      />
+    </div>;
+  }
+}
