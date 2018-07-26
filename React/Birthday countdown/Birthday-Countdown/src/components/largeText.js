2a049d62f84e5614a83cc33f09346c47c8fd5599
Jordan Hudgens <jordan@devcamp.com>
2018-07-26 13:28:32 -0600

    Large text updating, countdown has a bug

--- React/Birthday countdown/Birthday-Countdown/src/components/largeText.js
+++ React/Birthday countdown/Birthday-Countdown/src/components/largeText.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const LargeText = text => {
-  return <div className="large-text">{text}</div>;
+  return (
+    <div key={2} className="large-text">
+      {text}
+    </div>
+  );
 };
 export default LargeText;
